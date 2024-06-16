3934057948d66dd3e9e0a28a1442ba849b056988
Amber Welby <amberwelby@trentu.ca>
2024-06-16 11:51:52 -0700

    Correct linting errors

--- pages/_error.tsx
+++ pages/_error.tsx
@@ -1,22 +1,21 @@
 // CSS
 import styles from '../styles/Error.module.scss';
-import NavBar from "../components/NavBar";
-
+import NavBar from '../components/NavBar';
 
 export default function ErrorPage() {
   // Return error screen
   return (
     <div className={styles.canvas}>
-      <NavBar currentPage="Home" />
+      <NavBar currentPage='Home' />
       <div className={styles.page}>
         <div className={styles.container}>
-        <h1 className={styles.heading}>
-        404 PAGE NOT FOUND
-        </h1>
-        <p>
-        Oops! This page is missing or being developed, sorry! <br/>
-        Head back, or to [link to home] and choose a new adventure :)
-        </p>
+          <h1 className={styles.heading}>
+          404 PAGE NOT FOUND
+          </h1>
+          <p>
+          Oops! This page is missing or being developed, sorry! <br/>
+          Head back, or to [link to home] and choose a new adventure :)
+          </p>
         </div>
       </div>
     </div>
