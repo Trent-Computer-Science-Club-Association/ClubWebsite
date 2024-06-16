548b69943986b48049610e737635bf7e23a87652
Amber Welby <amberwelby@trentu.ca>
2024-06-16 11:44:42 -0700

    Add nav bar and colour

--- pages/_error.tsx
+++ pages/_error.tsx
@@ -1,17 +1,24 @@
 // CSS
 import styles from '../styles/Error.module.scss';
+import NavBar from "../components/NavBar";
+
 
 export default function ErrorPage() {
   // Return error screen
   return (
-    <div className={styles.container}>
-      <h1 className={styles.heading}>
-      404 PAGE NOT FOUND
-      </h1>
-      <p className={styles.content}>
-      Oops! This page is missing or being developed, sorry! <br/>
-      Head back, or to [link to home] and choose a new adventure :)
-      </p>
+    <div className={styles.canvas}>
+      <NavBar currentPage="Home" />
+      <div className={styles.page}>
+        <div className={styles.container}>
+        <h1 className={styles.heading}>
+        404 PAGE NOT FOUND
+        </h1>
+        <p>
+        Oops! This page is missing or being developed, sorry! <br/>
+        Head back, or to [link to home] and choose a new adventure :)
+        </p>
+        </div>
+      </div>
     </div>
   );
 }
