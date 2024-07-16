72bc77136c15f8e57fdfeb0ce0ae8d39fcc4dc42
Kara <kara.wilson.2005.08@gmail.com>
2024-07-15 20:47:21 -0400

    Fix(Button): Add Button Styles

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import Link from 'next/link';
 
+enum ButtonType {
+  NAVBAR = '',
+  LIGHT = 'light-btn',
+  SOCIAL = 'social-btn',
+}
+
 interface Props {
   onClick?: () => void;
   href?: string;
   className?: string;
+  type: ButtonType;
 }
 
 interface LabelProps extends Props {
@@ -21,13 +28,14 @@ const Button: React.FC<LabelProps | ChildrenProps> = ({
   onClick,
   href,
   className,
+  type,
 }) => {
   return href ? (
     <Link href={href}>
-      <span className={`${className}`}>{label || children}</span>
+      <span className={`${className} ${type}`}>{label || children}</span>
     </Link>
   ) : (
-    <button onClick={onClick} className={`${className}`}>
+    <button onClick={onClick} className={`${className} ${type}`}>
       {label || children}
     </button>
   );
