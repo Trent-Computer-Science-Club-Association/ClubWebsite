b9d18e6dfb393cdfaf9bde4f4ee2ddd5c365b50b
Amber Welby <amberwelby@trentu.ca>
2024-07-15 18:31:56 -0700

    Increase reusability

--- pages/_error.tsx
+++ pages/_error.tsx
@@ -2,22 +2,45 @@
 import styles from '../styles/Error.module.scss';
 import NavBar from '../components/NavBar';
 
-export default function ErrorPage() {
-  // Return error screen
+export default function ErrorPage({ 
+    statusCode 
+  }:{
+    statusCode: any
+  }) {
   return (
     <div className={styles.canvas}>
       <NavBar currentPage='Home' />
       <div className={styles.page}>
         <div className={styles.container}>
           <h1 className={styles.heading}>
-          404 PAGE NOT FOUND
+          Oops!
           </h1>
           <p>
-          Oops! This page is missing or being developed, sorry! <br/>
-          Head back, or to [link to home] and choose a new adventure :)
+            {statusCode
+              ? `An error ${statusCode} occurred on server`
+              : 'An error occurred on client'}
           </p>
+          <div>
+            <button
+              type='button'
+              className={styles.return}
+            >
+              Home
+            </button>
+          </div>
         </div>
       </div>
     </div>
-  );
+  )
+}
+ 
+ErrorPage.getInitialProps = ({ 
+    res, 
+    err 
+  }:{
+    res: any,
+    err: any,
+  }) => {
+  const statusCode = res ? res.statusCode : err ? err.statusCode : 404
+  return { statusCode }
 }
