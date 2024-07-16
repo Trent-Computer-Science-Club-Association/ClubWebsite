704ed954b5ee7f443af16f44fb5242ba3c23b629
Kara <kara.wilson.2005.08@gmail.com>
2024-07-15 20:09:32 -0400

    Fix(HeroBanner): Formatting

--- components/HeroBanner.tsx
+++ components/HeroBanner.tsx
@@ -19,12 +19,14 @@ export default function HeroBanner({ imagePath }: Props) {
       <div className={styles.leftColumn}>
         {/* square with ./public/images/logo.svg in it */}
         {/* <img src={imagePath} alt="TCSCA Logo" /> */}
-        <Image src={imagePath} alt="TCSCA Logo" width={100} height={100} />
+        <Image src={imagePath} alt='TCSCA Logo' width={100} height={100} />
         <Button>Apply</Button>
       </div>
       {/* Right Column */}
       <div className={styles.rightColumn}>
-        <div className={styles.title}>Trent Computer Science Club Association</div>
+        <div className={styles.title}>
+          Trent Computer Science Club Association
+        </div>
         <div className={styles.divider}></div>
         <div className={styles.tagline}>Where passion meets code</div>
         <div className={styles.socialMedia}>
