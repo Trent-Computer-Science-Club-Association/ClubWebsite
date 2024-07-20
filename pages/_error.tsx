5b008326b5e6ba010b113cf984af922733ce4c56
Amber Welby <amberwelby@trentu.ca>
2024-07-20 12:06:58 -0700

    Use button component

--- pages/_error.tsx
+++ pages/_error.tsx
@@ -1,12 +1,13 @@
 // CSS
 import styles from '../styles/Error.module.scss';
 import NavBar from '../components/NavBar';
+import Button, { ButtonType } from '../components/Button';
 
 export default function ErrorPage({ 
-    statusCode 
-  }:{
-    statusCode: any
-  }) {
+  statusCode 
+}:{
+  statusCode: any
+}) {
   return (
     <div className={styles.canvas}>
       <NavBar currentPage='Home' />
@@ -15,32 +16,27 @@ export default function ErrorPage({
           <h1 className={styles.heading}>
           Oops!
           </h1>
-          <p>
+          <p className={styles.body}>
             {statusCode
               ? `An error ${statusCode} occurred on server`
               : 'An error occurred on client'}
           </p>
           <div>
-            <button
-              type='button'
-              className={styles.return}
-            >
-              Home
-            </button>
+            <Button type={ButtonType.LIGHT}>Home</Button>
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
  
 ErrorPage.getInitialProps = ({ 
-    res, 
-    err 
-  }:{
-    res: any,
-    err: any,
-  }) => {
-  const statusCode = res ? res.statusCode : err ? err.statusCode : 404
-  return { statusCode }
-}
+  res, 
+  err 
+}:{
+  res: any,
+  err: any,
+}) => {
+  const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
+  return { statusCode };
+};
