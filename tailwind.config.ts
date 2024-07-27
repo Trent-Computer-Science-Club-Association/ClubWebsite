de4bbbeacedd684de7b467267445104d2fcd6c6a
Spotandjake <spotandjake@hotmail.com>
2024-07-26 21:18:17 -0400

    chore(Config): Run formatter

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -24,16 +24,16 @@ const config: Config = {
         socials: '#7BD4CB', // Social
       },
       aspectRatio: {
-        'event': '2 / 1', // This controls the aspect ratio of event elements, do not touch you will probably break stuff.
+        event: '2 / 1', // This controls the aspect ratio of event elements, do not touch you will probably break stuff.
       },
       boxShadow: {
         // Box Shadow - based on https://codepen.io/sdthornton/pen/wBZdXq
-        'l1': '0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)',
-        'l1Hover': '0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22)'
+        l1: '0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)',
+        l1Hover: '0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22)',
       },
       background: {
-        'aero': 'linear-gradient(0deg, rgba(0, 0, 0, .55) 0%, rgba(75, 75, 75, .55) 20%)'
-      }
+        aero: 'linear-gradient(0deg, rgba(0, 0, 0, .55) 0%, rgba(75, 75, 75, .55) 20%)',
+      },
     },
   },
   plugins: [],
