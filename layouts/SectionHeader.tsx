26bbdd5259795beed005d526cbbf760499368634
Kara <kara.wilson.2005.08@gmail.com>
2024-07-30 18:39:18 -0400

    Fix(SectionHeader): Move to style logic

--- layouts/SectionHeader.tsx
+++ layouts/SectionHeader.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from '../styles/components/SectionHeader.module.scss';
+import styles from '../styles/layouts/SectionHeader.module.scss';
 
 const SectionLocation = {
   RIGHT_ALIGNED: 'justify-end',
