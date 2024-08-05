8a1955458a24224f4735ed0b1b36e655117785ba
Kara <kara.wilson.2005.08@gmail.com>
2024-08-05 10:55:29 -0400

    Chore(Index): Add comments

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,6 +17,7 @@ export default function Home() {
       <section className={styles.container}>
         <main className={styles.MainArea}>
           <HeroBanner />
+          {/* In the config we have a list of sections, this is how we iterate through them */}
           {homeSections.map((section, index) => (
             <section key={index}>
               <Section title={section.title} index={index}>
@@ -32,6 +33,7 @@ export default function Home() {
               </Section>
             </section>
           ))}
+          {/* News Section */}
           <Section title='Latest News' index={homeSections.length}>
             <NewsSection newsFeed={newsFeed.items} />
           </Section>
