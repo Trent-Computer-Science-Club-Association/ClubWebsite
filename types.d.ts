32220810f00f3aa96cc0be6b444eb50247657b99
Spotandjake <spotandjake@hotmail.com>
2024-08-16 22:22:28 -0400

    chore: comment about `types.d.ts`

--- types.d.ts
+++ types.d.ts
@@ -1,5 +1,6 @@
 // Yaml Config
 declare module '*.yaml' {
+  // This is marked as unknown because when we import from the yaml we do not know any of the type information.
   const value: unknown;
   export default value;
 }
