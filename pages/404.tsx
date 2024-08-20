f2ae0f8fd342917c7341116c87bc39db90122eea
Amber Welby <98117997+amberwelby@users.noreply.github.com>
2024-08-19 20:57:40 -0700

    feat(404 Page): Create custom 404 page (#14)
    Adding a set of error pages, notably generic and 404 pages, to cover as many potential issues as possible. Depending on the error, users may return home or report the issue on GitHub.

---
+++ pages/404.tsx
@@ -0,0 +1,13 @@
+// Components
+import ErrorLayout from '../layouts/Error';
+
+export default function ErrorPage() {
+  return (
+    <ErrorLayout>
+      <p>
+        404 - Page not found <br />
+        This page may be in development or may not exist at this time
+      </p>
+    </ErrorLayout>
+  );
+}
---
+++ pages/_error.tsx
@@ -0,0 +1,39 @@
+// Components
+import type { NextPageContext } from 'next';
+import ErrorLayout from '../layouts/Error';
+import Image from '../components/Image';
+
+interface Props {
+  statusCode: number;
+}
+
+export default function ErrorPage({ statusCode }: Props) {
+  return (
+    <ErrorLayout>
+      <p>
+        {statusCode != 418
+          ? `An error occured with status code ${statusCode}`
+          : 'An unknown error occured'}
+        {statusCode == 418 && teapot()}
+      </p>
+    </ErrorLayout>
+  );
+}
+
+// Get error information
+ErrorPage.getInitialProps = ({ res, err }: NextPageContext) => {
+  const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
+  return { statusCode };
+};
+
+// Easter Egg
+function teapot() {
+  return (
+    <>
+      <p>If you're looking for coffee you're in the wrong place</p>
+      <div>
+        <Image src='/teapot.svg' alt='Teapot outline' />
+      </div>
+    </>
+  );
+}
