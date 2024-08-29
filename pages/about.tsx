199462c1c8ebb9e4e40273a515c21c5173d3e839
Amber Welby <amberwelby@trentu.ca>
2024-08-29 09:36:33 -0700

    Initial structure and styling (error importing config)

---
+++ pages/about.tsx
@@ -0,0 +1,30 @@
+import styles from '../styles/About.module.scss';
+import { about_page } from '../config';
+import NavBar from '../components/NavBar';
+import EventBanner from '../components/EventBanner';
+import HeroBanner from '../components/HeroBanner';
+import Footer from '../components/Footer';
+import Section from '../layouts/Section';
+
+export default function About() {
+  const { sections } = about_page;
+  
+  return (
+    <>
+      <EventBanner />
+      <NavBar currentPage='About' />
+      <section className={styles.container}>
+        <main className={styles.MainArea}>
+          <HeroBanner />
+          {/* Sections found in the config */}
+          {sections.map((section, i) => (
+            <section key={i}>
+              <Section sectionConfig={section} index={i} />
+            </section>
+          ))}
+        </main>
+        <Footer />
+      </section>
+    </>
+  )
+}
