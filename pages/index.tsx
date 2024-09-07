d875cf0b000410c5e0b1040846217167093a82c3
Spotandjake <spotandjake@hotmail.com>
2024-09-07 19:27:17 -0400

    chore: changes after rebase on main

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Head from 'next/head';
 import styles from '../styles/Home.module.scss';
-import { home_page } from '../config';
+import { website_config, home_page } from '../config';
 // Internal Components
 import NavBar from '../components/NavBar';
 import EventBanner from '../components/EventBanner';
@@ -16,10 +16,9 @@ Modal.setAppElement('#__next'); // Set the app element for accessibility
 export default function Home() {
   const { sections } = home_page;
 
+  // In the config we have a list of sections, this is how we iterate through them
   const renderSections = sections.map((section, i) => (
-    <section key={i}>
-      <Section sectionConfig={section} index={i} />
-    </section>
+    <Section sectionConfig={section} index={i} key={i} />
   ));
 
   return (
@@ -32,10 +31,7 @@ export default function Home() {
       <section className={styles.container}>
         <main className={styles.MainArea}>
           <HeroBanner />
-          {/* In the config we have a list of sections, this is how we iterate through them */}
-          {sections.map((section, i) => (
-            <Section sectionConfig={section} index={i} key={i} />
-          ))}
+          {renderSections}
         </main>
         <Footer />
       </section>
